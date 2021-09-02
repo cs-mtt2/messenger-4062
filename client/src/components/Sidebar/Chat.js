b347d24b2cd19974953eb710b4df61d1a30f5b30
Michael Tran <michaeltran20052006@yahoo.com>
2021-09-02 16:07:19 -0500

    Implemented UI update for using read messages functionality

--- client/src/components/Sidebar/Chat.js
+++ client/src/components/Sidebar/Chat.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import { Box, Chip } from "@material-ui/core";
 import { BadgeAvatar, ChatContent } from "../Sidebar";
 import { makeStyles } from "@material-ui/core/styles";
 import { setActiveChat } from "../../store/activeConversation";
@@ -16,17 +16,23 @@ const useStyles = makeStyles((theme) => ({
     "&:hover": {
       cursor: "grab"
     }
+  },
+  notification: {
+    marginLeft: 20,
+    marginRight: 20,
   }
 }));
 
 const Chat = (props) => {
   const classes = useStyles();
-  const { conversation } = props;
-  const { otherUser } = conversation;
+  const { conversation, activeConversation } = props;
+  const { otherUser, unreadMessages } = conversation;
 
   const handleClick = async (conversation) => {
     await props.setActiveChat(conversation.otherUser.username);
   };
+  
+  const shouldDisplayNotification = unreadMessages && unreadMessages.length > 0 && activeConversation !== conversation.otherUser.username;
 
   return (
     <Box onClick={() => handleClick(conversation)} className={classes.root}>
@@ -37,6 +43,9 @@ const Chat = (props) => {
         sidebar={true}
       />
       <ChatContent conversation={conversation} />
+      {shouldDisplayNotification && (
+          <Chip className={classes.notification} size="small" color="primary" label={unreadMessages.length}/>
+      )}
     </Box>
   );
 };
