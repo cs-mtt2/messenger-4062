e827e55c5a8861caab0f8e22bba136732b4a0c52
Michael Tran <michaeltran20052006@yahoo.com>
2021-09-02 16:03:37 -0500

    Added boolean flag for read messages  server side

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -13,7 +13,12 @@ router.post("/", async (req, res, next) => {
 
     // if we already know conversation id, we can save time and just add it to message and return
     if (conversationId) {
-      const message = await Message.create({ senderId, text, conversationId });
+      const message = await Message.create({
+        senderId,
+        text,
+        conversationId,
+        readByReceipient: false,
+      });
       return res.json({ message, sender });
     }
     // if we don't have conversation id, find a conversation to make sure it doesn't already exist
@@ -36,8 +41,40 @@ router.post("/", async (req, res, next) => {
       senderId,
       text,
       conversationId: conversation.id,
+      readByReceipient: false,
     });
     res.json({ message, sender });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// expects { messages } in body
+router.put("/", async (req, res, next) => {
+  try {
+    if (!req.user) {
+      return res.sendStatus(401);
+    }
+    const { messages } = req.body;
+
+    console.log("BODY", req.body);
+    const messageIds = messages.map((message) => {
+      return message.id;
+    });
+
+    // Returns amount of rows updated and the message that was updated in the form of array [rowsUpdated, Message]
+    const updateArray = await Message.update(
+      { readByRecipient: true },
+      { returning: true, where: { id: messageIds } }
+    );
+
+    const jsonToReturn = {
+      messages: updateArray[1],
+    };
+
+    console.log("server", updateArray);
+
+    res.json(jsonToReturn);
   } catch (error) {
     next(error);
   }
